8b25d9917cb3628cdfb9db2fc6476e65640f1a2f
Noah A Klem <noahaklem@gmail.com>
2023-09-25 14:52:00 -0600

    sinatra basics

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Noah"
+    end
+
+    get '/hometown' do 
+        "My hometown is Florence, MT."
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is nothing."
+    end
 end
